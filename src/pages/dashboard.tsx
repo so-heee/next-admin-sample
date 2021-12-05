b5b6a967713d4299e6a5d64cf8243e226d9c016f
so-heee <so.heee.mochi@gmail.com>
2021-12-05 10:42:31 +0900

    テンプレートを追加

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,9 @@
+import { DashboardLayout } from '../components/templates/dashboard/DashboardLayout'
+
+export default function Index(): JSX.Element {
+  return (
+    <DashboardLayout title="トップページ">
+      <>トップページ内容</>
+    </DashboardLayout>
+  )
+}
